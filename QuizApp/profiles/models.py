227885a36f93097f288ebce897396eece5b9378d
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-11-07 01:33:39 +0300

    Small Group fix!

--- QuizApp/profiles/models.py
+++ QuizApp/profiles/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 
 class Group(models.Model):
+    """Модель стеденческой группы"""
     FACULTY_CHOICES = (
         ('А', 'Ракетно-космической техники'),
         ('Е', 'Оружие и системы вооружения'),
@@ -16,19 +18,29 @@ class Group(models.Model):
         ('М', 'Магистратура'),
     )
     faculty = models.CharField(max_length=1, choices=FACULTY_CHOICES, default='А')
-    number = models.CharField(max_length=3, default='000')
+    number = models.CharField(max_length=3, default='000', unique=True)
     study_form = models.CharField(max_length=1, choices=STUDY_FORM_CHOICES, default='Б')
 
     def __str__(self) -> str:
         return f"{self.faculty}{self.number}{self.study_form}"
 
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
 
 class Profile(models.Model):
+    """Модель расширяюшая модель User"""
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
     group = models.ForeignKey(Group, related_query_name='profiles',
                                 blank=True, null=True, on_delete=models.SET_NULL)
+    is_teacher = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f"{self.user.first_name} {self.user.last_name} - {self.group}"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
 
 

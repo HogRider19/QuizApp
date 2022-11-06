024a26b959c1cec4dd4b71ada544f08ca5be55e1
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-11-07 02:41:18 +0300

    Added Cource model!

--- QuizApp/quiz/models.py
+++ QuizApp/quiz/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from profiles.models import Group, Profile
 
-# Create your models here.
+
+class Cource(models.Model):
+    name = models.CharField(max_length=250)
+    groups = models.ManyToManyField(Group, related_query_name='cources', through='CourceGroup')
+    teachers = models.ManyToManyField(Profile, related_query_name='author_courses', blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+class CourceGroup(models.Model):
+    cource = models.ForeignKey(Cource, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    time_joined = models.DateTimeField(auto_now_add=True)
+
+
+class Test(models.Model):
+    pass

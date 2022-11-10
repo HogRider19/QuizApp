78954987ddac5625ada00e2d3869d7e092bb5437
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-11-10 13:53:43 +0300

    Added courcedetail and testdetail views!

--- QuizApp/quiz/urls.py
+++ QuizApp/quiz/urls.py
@@ -2,5 +2,7 @@ from django.urls import  path
 from . import views
 
 urlpatterns = [
-    path('home/', views.HomePage.as_view(), name='home'),
+    path('cource/<int:pk>/', views.CourceDetailView.as_view(), name='courcedetail'),
+    path('test/<int:pk>/', views.TestDetailView.as_view(), name='testdetail'),
+    path('', views.HomePage.as_view(), name='home'),
 ]
--- QuizApp/quiz/views.py
+++ QuizApp/quiz/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.views import generic
-from .models import Cource
+from .models import Cource, Test
 
 
 class HomePage(generic.ListView):
@@ -11,6 +11,16 @@ class HomePage(generic.ListView):
 
     def get_queryset(self):
         return self.request.user.profile.group.cources.all()
+
+class CourceDetailView(generic.DetailView):
+    model = Cource
+    template_name = 'quiz/courcedetail.html'
+    context_object_name = 'cource'
+
+class TestDetailView(generic.DetailView):
+    model = Test
+    template_name = 'quiz/testdetail.html'
+    context_object_name = 'test'
 
 
 

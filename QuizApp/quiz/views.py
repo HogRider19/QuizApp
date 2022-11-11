e8b7642dc6243c5c996a59797b29d8cadcfd4cbe
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-11-12 01:23:07 +0300

    testdetail view!

--- QuizApp/quiz/views.py
+++ QuizApp/quiz/views.py
@@ -5,6 +5,9 @@ from .models import Cource, Test
 from django.contrib.auth.mixins import UserPassesTestMixin
 from .permissions import OnlyAuthenticatedPermission
 from typing import *
+import datetime
+from django.utils import timezone
+
 
 class HomePage(OnlyAuthenticatedPermission, generic.ListView):
     model = Cource
@@ -13,7 +16,6 @@ class HomePage(OnlyAuthenticatedPermission, generic.ListView):
 
     def get_queryset(self):
         return self.request.user.profile.group.cources.all()
-
 
 class CourceDetailView(OnlyAuthenticatedPermission, generic.DetailView):
     model = Cource
@@ -24,6 +26,15 @@ class TestDetailView(OnlyAuthenticatedPermission, generic.DetailView):
     model = Test
     template_name = 'quiz/testdetail.html'
     context_object_name = 'test'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        at_start = context.get('test').at_start
+        at_finish = context.get('test').at_finish
+        time_now = datetime.datetime.now(at_start.tzinfo)
+        status = 'open' if at_start < time_now < at_finish else 'close'
+        context.update({'status': status})
+        return context
 
 
 
